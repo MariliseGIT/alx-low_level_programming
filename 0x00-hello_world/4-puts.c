963b2efce4c9276167f2c3492b4ad34c08b46bbc
MariliseGIT <vwykmarilise@gmail.com>
2023-02-16 10:03:52 -0800

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-include <stdio.h>
-int main() 
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ *  Return: 0 (success)
+ */
+int main(void)
 {
-	char string [] = "Programming is like building a multilingual puzzle";
-	puts (string)
-	
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
