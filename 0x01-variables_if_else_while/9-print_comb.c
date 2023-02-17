28266367677d8fb98360980745dc6e79553c49ad
MariliseGIT <vwykmarilise@gmail.com>
2023-02-17 13:42:56 -0800

    patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: Print all possible combinations of single-digit numbers.
+ * Print numbers in ascending order and separate by , and a space
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	if (d 1= '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
