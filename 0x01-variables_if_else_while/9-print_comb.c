e25d95dd35d759728ca662609f49492bc7a9c7bf
MariliseGIT <vwykmarilise@gmail.com>
2023-02-17 15:29:25 -0800

    fix error output code

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,16 +12,15 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 48 ; i <= 58; i++)
 	{
 		putchar(i);
 	}
-	if (i != '9')
+	if (i != 57)
 	{
 		putchar(',');
 		putchar(' ');
 	}
-	i++;
 	putchar('\n');
 	return (0);
 }
