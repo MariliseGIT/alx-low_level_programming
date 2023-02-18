c5358da066f4920b59fe7ecbfaed874e91e776a1
MariliseGIT <vwykmarilise@gmail.com>
2023-02-17 17:47:05 -0800

    betty error fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,8 +18,8 @@ int main(void)
 
 		for (j = 0; j < 100; j++)
 		{
-		c = j /10;  /* doubles snum */ 
-		d = j % 10; /* singles snum */ 
+		c = j /10;  /*  doubles snum */
+		d = j % 10; /* singles snum */
 
 			if (a < c || (a == c && b < d))
 			{
