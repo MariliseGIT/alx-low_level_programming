9cb7b0083f980e332297cc714781a1128b32e962
MariliseGIT <vwykmarilise@gmail.com>
2023-02-21 11:25:31 -0800

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Print the alphabet in lowercase followed by a new line.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char alphabet;
+	
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabte++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
