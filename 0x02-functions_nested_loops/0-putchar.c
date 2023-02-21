e20377f7032f879fc2a65bc4b331c9773fc2d906
MariliseGIT <vwykmarilise@gmail.com>
2023-02-21 08:26:17 -0800

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Print _putchar followed by a new line with a return of 0
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(word[i]);
+	}
+	putchar('\n');
+	return (0);
+}
