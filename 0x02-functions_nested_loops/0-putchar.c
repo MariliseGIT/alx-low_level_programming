ec6ccc136b4e4613884a07bb8257bbd62fcc17e2
MariliseGIT <vwykmarilise@gmail.com>
2023-02-21 09:24:24 -0800

    correction file 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  *
  * Return: 0 (success)
  */
-int _putchar(char c); {
+int _putchar(char c) {
 	return putchar(c);
 }
 int main(void)
@@ -18,7 +18,7 @@ int main(void)
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_pucthar('r');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
