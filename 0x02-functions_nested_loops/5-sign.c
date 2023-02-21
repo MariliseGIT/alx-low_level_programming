3c39272c0ba7850879d4ed100a2031ca4103edcc
MariliseGIT <vwykmarilise@gmail.com>
2023-02-21 14:00:24 -0800

    file changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,12 +6,12 @@
  * @n: The number of whic the sign will be printed.
  *
  * Return: 1 if the number is greater than zero,
- * 	0 if the number is zer,
- * 		-1 if the number is less than zero.
+ * 0 if the number is zer,
+ * -1 if the number is less than zero.
  */
 int print_sign(int n)
 {
-	if (n >0)
+	if (n > 0)
 	{
 		putchar('+');
 		return (1);
@@ -24,6 +24,6 @@ int print_sign(int n)
 	else
 	{
 		putchar('-');
-		return(-1);
+		return (-1);
 	}
 }
