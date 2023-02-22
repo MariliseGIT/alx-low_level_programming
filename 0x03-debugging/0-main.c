0e4f9c7f68f94b8bd760f6d68feffd2944dd7ec6
MariliseGIT <vwykmarilise@gmail.com>
2023-02-22 01:59:44 -0800

    added files 0-main.c and 1-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - test function that prints if integer is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_pr_negative(i);
+
+	return (0);
+}
