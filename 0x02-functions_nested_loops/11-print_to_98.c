57136fe01c103d1cf0cd5dde49db34435ebd2d34
MariliseGIT <vwykmarilise@gmail.com>
2023-02-21 16:58:38 -0800

    file corrected

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,22 +17,23 @@ void print_to_98(int n)
 			printf("%d", n);
 
 			if ( n == 98)
-				continue;
-			printf(", ");
+				continue
+			printf(", ")
 		}
-	}else
+	else
 			printf("\n");
 	}
-	( else
+	else
 	{
 		for (; n >= 98; n--)
 		{
 			printf("d", n);
 
 			if (n == 98)
-				continue;
-			printf(", ");
+				continue
+			printf(", ")
 		}
 		printf("\n");
-	})
+	}
+
 }
