9b63d34ce52eb20d642a66605b896d9fde834368
MariliseGIT <vwykmarilise@gmail.com>
2023-02-23 06:42:07 -0800

    added files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - check main
+ * @c: The character to be checked.
+ * Description: function that checks for an uppercase character.
+ *
+ * Return: 1 if chacter is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c<= 'Z')
+		return (1);
+	else
+		return (0);
+}
