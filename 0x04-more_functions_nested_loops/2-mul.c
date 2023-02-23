35ffe9a91cd46899a75334e4489ab5bba30faed0
MariliseGIT <vwykmarilise@gmail.com>
2023-02-23 09:46:02 -0800

    errors fix

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * mul: check main
+ * mul - check main
  * @a: The first integer to be multiplied.
  * @b: The second integer to be multiplied.
  * Description: mul function multiplies two integers.
