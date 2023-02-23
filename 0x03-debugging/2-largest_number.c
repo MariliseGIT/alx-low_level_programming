5dd8de4f840a717a47d1047b388cadf96b52ee3a
MariliseGIT <vwykmarilise@gmail.com>
2023-02-23 09:34:04 -0800

    fix erro

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * largets_number - it is to returns the largest of 3 numbers
+ * largets_number -check main
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ * Description: Print out the largest of three numbers, no matter the case.
  * Return: largest number
  */
 int largest_number(int a, int b, int c)
