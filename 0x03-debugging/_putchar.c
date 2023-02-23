895e95d8c19ec4f5cf80923dda09e8e1ed5303e3
MariliseGIT <vwykmarilise@gmail.com>
2023-02-23 09:25:17 -0800

    add file

--- 0x03-debugging/_putchar.c
+++ 0x03-debugging/_putchar.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * _putchar - wites the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On succes 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ *
+ */
+int _pytchar(char c)
+{
+	return (write(1, &c, 1));
+}
