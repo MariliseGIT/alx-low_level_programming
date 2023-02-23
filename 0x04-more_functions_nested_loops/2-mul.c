f1eebb1af72155d05733923d2c4c588fb997836d
MariliseGIT <vwykmarilise@gmail.com>
2023-02-23 06:55:31 -0800

    added files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * mul: check main
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
+ * Description: mul function multiplies two integers.
+ *
+ * Return: The result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_numbers - check main
+ * Description: print the numbers from 0-9, followed by a new line.
+ *
+ * Return: 0 (success)
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0, num <= 9; num ++)
+		_putchar((num % 10) + '0');
+	_putchar('\n');
+	return (0);
+}
