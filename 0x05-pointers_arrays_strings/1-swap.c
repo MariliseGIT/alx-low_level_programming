0505b72b14d3339563ba85f4608b84170abb9260
MariliseGIT <vwykmarilise@gmail.com>
2023-02-27 04:40:35 -0800

    compiler error fix 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,9 +6,9 @@
  * @a: The first integer to be swapped.
  * @b: The second integer to be swapped.
  */
-void swap_int(int *x, int *b)
+void swap_int(int *a, int *b)
 {
 	int tmp = *a;
 	*a = *b;
-	*b = tmp:
+	*b = tmp;
 }
