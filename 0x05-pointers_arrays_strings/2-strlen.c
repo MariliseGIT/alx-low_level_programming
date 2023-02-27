54acdce5d72ddfb5297cebd78d34fbab413251ed
MariliseGIT <vwykmarilise@gmail.com>
2023-02-27 04:50:24 -0800

    compiler error fix 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.c>
+/**
+ * _strlen - Returns the length of a sting.
+ * @s: char
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
