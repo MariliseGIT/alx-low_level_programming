38253c126bfb3f2a76e2a1af575b8ca71f6f4fce
MariliseGIT <vwykmarilise@gmail.com>
2023-03-01 06:52:02 -0800

    files added

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * cap_string - Capitalize all words of a string.
+ * @s: string
+ * Return: the address of s.
+ */
+char *cap_string(char *s)
+{
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + i) >= 'a' && *(s + i) <= 'z')
+	{
+		if (i == 0)
+			*(s + i) -= 'a' - 'A';
+		else
+		{
+			for (j = 0; j <= 12; j++)
+			{
+			if (a[j] == *(s + i - 1))
+				*(s + i) -= 'a' - 'A';
+			}
+		}
+	}
+	return (s);
+}
+
