139e232a3730d3e82446dc8b3448b5555caaf742
MariliseGIT <vwykmarilise@gmail.com>
2023-03-06 05:36:59 -0800

    changed code 8-print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,17 +8,21 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum1 = 0, sum2 = 0;
+	int i, s1 = 0, s2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + i * size + i);
+		s1 += a[i];
+		a += size;
 	}
 
-	for (i = 0, j = size - 1; i < size; i++, j--)
+	a -= size;
+
+	for (i = 0; i < size; i++)
 	{
-		sum2 += *(a + i * size + j);
+		s2 += a[i];
+		a -= size;
 	}
-	printf("Sum of main diagonal: %d\n", sum1);
-	printf("Sum of secondary diagonal: %d\n", sum2);
+
+	print("%d, &d\n", s1, s2);
 }
