2325ea94811c24e167f600ec267fb453a4071ddb
MariliseGIT <vwykmarilise@gmail.com>
2023-03-10 12:38:20 -0800

    file added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Print the prgram name, folled by a new line.
+ * @argc: the number of arguments supplied to the program
+ * @argv: an array of pointers to the arguments
+ * Return: 0 Always.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0):
+}
