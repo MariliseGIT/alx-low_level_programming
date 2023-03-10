8c1a2cbde12cb5a86251c3fc9f37bd2064db3ffa
MariliseGIT <vwykmarilise@gmail.com>
2023-03-10 09:57:52 -0800

    files added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Display name of a program.
+ * @argc: int
+ * @argv:string
+ * Return:int
+ */
+int main(int argc, char *argv[]__attibute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
