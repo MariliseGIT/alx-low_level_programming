6d7f3e236d690abf1c52caa796be09d1941267c7
MariliseGIT <vwykmarilise@gmail.com>
2023-03-14 07:56:44 -0700

    code error fix 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,32 +3,35 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+
+int wordcounter(char *str, int pos, char fchar);
+char **strtow(char *str);
 /**
- * wordcounter - counts words and the letters in them
- * @str: string to count
- * @pos: position of the word to count characters from
- * @firstchar: position of the first letter of the word
- * if pos = 0, count the number of chars in the word
- * else count number of words
- * Return: wordcount if pos == 0,
- * length of word if pos > 0,
- * position of word if pos > 0 && firstchar > 0
+ * wordcounter - Count the words and letters in string.
+ * @str: string
+ * @pos: position of word to count characters form
+ * @fchar: position of first letter
+ * Definition: If pos = 0, count the numbers of chars in words,
+ * else count the number of words.
+ * Return: wordcount if pos == 0, lenght of word if pos > 0,
+ * position of pord if pos > 0 && fchar > 0.
  */
-int wordcounter(char *str, int pos, char firstchar)
+int wordcounter(char *str, int pos, char fchar)
 {
 	int i, wordcount, charcount, flag;
 
 	str[0] != ' ' ? (wordcount = 1) : (wordcount = 0);
 	for (i = 0, flag = 0; str[i]; i++)
 	{
-		if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0' && flag == 0)
+		if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0' &&
+				flag == 0)
 		{
 			wordcount++;
 			flag = 1;
 		}
 		if (pos > 0 && pos == wordcount)
 		{
-			if (pos > 0 && pos == wordcount && firstchar > 0)
+			if (pos > 0 && pos == wordcount && fchar > 0)
 				return (i);
 			for (charcount = 0; str[i + charcount + 1] != ' '; charcount++)
 				;
@@ -40,13 +43,13 @@ int wordcounter(char *str, int pos, char firstchar)
 	return (wordcount);
 }
 /**
- * strtow - convert a string into a 2d array of words
- * @str: string to convert
- * Return: double pointer to 2d array
+ * strtow - Convert string into array of words.
+ * @str: string
+ * Return: double pointer to array.
  */
 char **strtow(char *str)
 {
-	int wc, wordlen, getfirstchar, len, i, j;
+	int wc, wordlen, getfchar, len, i, j;
 	char **p;
 
 	for (len = 0; str[len]; len++)
@@ -61,26 +64,24 @@ char **strtow(char *str)
 		return (NULL);
 	for (i = 0, wordlen = 0; i < wc; i++)
 	{
-		/* Allocate memory for nested elements */
 		wordlen = wordcounter(str, i + 1, 0);
 		if (i == 0 && str[i] != ' ')
 			wordlen++;
 		p[i] = malloc(wordlen * sizeof(char) + 1);
 		if (p[i] == NULL)
 		{
-			for ( ; i >= 0; --i)
+			for (; i >= 0; i--)
 				free(p[i]);
 			free(p);
 			return (NULL);
 		}
-		/* initialize each element of the nested array with the word*/
-		getfirstchar = wordcounter(str, i + 1, 1);
+		getfchar = wordcounter(str, i + 1, 1);
 		if (str[0] != ' ' && i > 0)
-			getfirstchar++;
+			getfchar++;
 		else if (str[0] == ' ')
-			getfirstchar++;
+			getfchar++;
 		for (j = 0; j < wordlen; j++)
-			p[i][j] = str[getfirstchar + j];
+			p[i][j] = str[getfchar + j];
 		p[i][j] = '\0';
 	}
 	p[i] = NULL;
