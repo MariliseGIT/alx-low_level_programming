8e770bb0d48437099b5e18b983ed484e202aa9d5
MariliseGIT <vwykmarilise@gmail.com>
2023-03-21 12:42:19 -0700

    code fix to 4-new+dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,24 +26,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	cpyname = malloc(len_name + 1);
-
-		if (cpyname == NULL)
+	if (cpyname == NULL)
 		return (NULL);
-		for (i = 0; name[i]; i++)
+	for (i = 0; name[i]; i++)
 		cpyname[i] = name[i];
-		cpyname[i] = '\0';
+	cpyname[i] = '\0';
 
 	cpyowner = malloc(len_owner + 1);
-
-		if (cpyowner == NULL)
+	if (cpyowner == NULL)
 		return (NULL);
-		for (i = 0; owner[i]; i++)
+	for (i = 0; owner[i]; i++)
 		cpyowner[i] = owner[i];
-		cpyowner[i] = '\0';
+	cpyowner[i] = '\0';
 
 	new_dog->name = cpyname;
 	new_dog->age = age;
 	new_dog->owner = cpyowner;
-	free(new_dog);
+	free(cpyname);
+	free(cpyowner);
 	return (new_dog);
 }
